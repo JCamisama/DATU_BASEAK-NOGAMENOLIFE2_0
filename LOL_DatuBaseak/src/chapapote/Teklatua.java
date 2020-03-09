9ac123954dd85b41c91dc392ecf5666a30c84af5
GySyX <GySyX@10.109.157.124>
2020-03-09 17:26:12 +0100

    Teklatua

---
+++ LOL_DatuBaseak/src/chapapote/Teklatua.java
@@ -0,0 +1,68 @@
+package chapapote;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Teklatua {
+	private static Teklatua nireTeklatua=null;
+	private Scanner sc;
+	
+	private Teklatua() {
+		this.sc=new Scanner(System.in);
+	}
+	public static Teklatua getTeklatua() {
+		if(nireTeklatua==null) {
+			nireTeklatua= new Teklatua();
+		}
+		return nireTeklatua;
+	}
+	
+	public void skipLine(String pMezua){
+		System.out.println(pMezua);
+		sc.nextLine();
+	}
+	
+	public int irakurriOsoaSalbuespen(String pMezua) {
+		System.out.println(pMezua+": ");
+		int zenbakia;
+		zenbakia=sc.nextInt();
+		return zenbakia;
+	}
+	
+	public int irakurriOsoa(String pMezua) {
+		int erantzuna=0;
+		boolean kontrolpean=false;
+		do{
+			try{
+				erantzuna=this.irakurriOsoaSalbuespen(pMezua);
+				kontrolpean=true;
+		}
+			catch (InputMismatchException e){
+				System.out.println("Sartu duzun balioa ez da zenbaki oso bat, saiatu berriz. ");
+				sc=new Scanner(System.in);
+			}
+	}while (!kontrolpean);
+		return erantzuna;
+	}	
+	
+	
+	//Metodo berriak
+	public String noraMugitu(String pMezua){
+		
+		System.out.print(pMezua+": ");
+		String erantzuna	= sc.next();
+		
+		
+		return erantzuna;
+	}
+	
+	public String hitzaIrakurri(String pMezua){
+		
+		System.out.print(pMezua+": ");
+		String erantzuna	= sc.next();
+		
+		
+		return erantzuna;
+	}
+	
+}
