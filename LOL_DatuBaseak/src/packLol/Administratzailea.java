9f2cccdcb3ed8bb8f4384fa599078b409910b4e9
Adrián Sáanchez <adriansanchez0123@gmail.com>
2020-03-09 17:21:37 +0100

    ff

--- LOL_DatuBaseak/src/packLol/Administratzailea.java
+++ LOL_DatuBaseak/src/packLol/Administratzailea.java
@@ -1,10 +1,38 @@
 package packLol;
 
+import java.sql.Connection;
+
+import chapapote.Jokoa;
+
 public class Administratzailea {
     public Administratzailea(){
         
     }
     public void menuaBistaratu(){
-        
+        int aukera = 0;
+        Connection konexioa=null;
+        while (aukera != 9) {
+            System.out.println("Ongietorri Splendor jokora. Zer egin nahi duzu?");
+            System.out.println("+--------------+");
+            System.out.println("|    Menua     |");
+            System.out.println("+--------------+");
+            System.out.println("Aukeratu:");
+            System.out.println("1.- Jokalari berria sartu");
+            System.out.println("2.- Partida berria sartu");
+            System.out.println("3.- Jokalari guztiak erakutsi");
+            System.out.println("9.- Irten");
+            System.out.println("Sartu aukera");
+            aukera=Teklatua.getTeklatua().irakurriOsoa("Sartu aukera");
+            //konexioa = Jokoa.konexioa();
+            if(aukera==1){
+                
+            }
+            else if(aukera==2){
+                
+            }
+            else if(aukera==3){
+                
+            }
+        }
     }
 }
